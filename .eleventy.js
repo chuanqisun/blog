1009ab00ff6f3fdbd647f36d0336503f759cf08d
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-08-01 09:49:53 -0700

    add rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,23 @@
-module.exports = function (eleventyConfig) {
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "rss",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "posts", // iterate over `collections.posts`
+      limit: 10, // 0 means no limit
+    },
+    metadata: {
+      language: "en",
+      title: "Stack Diver",
+      subtitle: "Inquiry of zeros and ones",
+      base: "https://blog.chuanqisun.com/",
+      author: {
+        name: "Chuanqi Sun",
+      },
+    },
+  });
   eleventyConfig.addPassthroughCopy("src/style.css");
   return {
     dir: {
@@ -6,4 +25,4 @@ module.exports = function (eleventyConfig) {
       output: "public",
     },
   };
-};
+}
